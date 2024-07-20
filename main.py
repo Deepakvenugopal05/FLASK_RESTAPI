231fc312782a20754f5859fdb7caf32037d5574d
Deepak venugopal <137137316+Deepakvenugopal05@users.noreply.github.com>
2024-07-20 17:58:52 +0530

    main.py

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ app.register_blueprint(department_bp, url_prefix='/department/')
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
-    app.run(debug=True, port=7000)
+    app.run(host='0.0.0.0', port=8888)
